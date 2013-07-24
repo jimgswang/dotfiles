b1b57b92ad1872ad0290ca2dd7de27a09fd4edda
Jim Wang <jimw923@hotmail.com>
2013-07-23 19:59:53 -0700

    Added Vim Pathogen install

--- setup.sh
+++ setup.sh
@@ -2,6 +2,14 @@
 # Jim Wang
 # jim.gs.wang@gmail.com
 
+#Install GNU Screen
+sudo apt-get install screen
+
+#Install Vim Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
 #symlink the included dotfiles
 cd $HOME
 ln -sb dev-env/.screenrc .

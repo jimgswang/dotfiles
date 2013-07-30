9044fed496ea585a3362992ce3c2dc4882404a11
Jim Wang <jimw923@hotmail.com>
2013-07-30 16:23:15 -0700

    added rlwrap for node repl

--- .bashrc
+++ .bashrc
@@ -86,6 +86,7 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
+alias node='env NODE_NO_READLINE=1 rlwrap node'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
--- setup.sh
+++ setup.sh
@@ -2,15 +2,22 @@
 # Jim Wang
 # jim.gs.wang@gmail.com
 
-#Install GNU Screen
-sudo apt-get install screen
+# Install GNU Screen
+sudo apt-get install -y screen
 
-#Install Vim Pathogen
+# Install Vim Pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-#symlink the included dotfiles
+# Install vim-slime
+cd ~/.vim/bundle
+git clone git://github.com/jpalardy/vim-slime.git
+
+# Install rlwrap
+sudo apt-get install -y rlwrap
+
+# symlink the included dotfiles
 cd $HOME
 ln -sb dev-env/.screenrc .
 ln -sb dev-env/.bashrc .

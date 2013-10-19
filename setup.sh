b61398b7c6c1f71895c1a0ddfc8190b5af7e18d1
Jim Wang <jim.gs.wang@gmail.com>
2013-10-19 06:58:27 +0000

    add bash_ind symlink

--- setup.sh
+++ setup.sh
@@ -22,7 +22,8 @@ sudo apt-get install -y rlwrap
 
 # symlink the included dotfiles
 cd $HOME
-ln -sb dev-env/.screenrc .
 ln -sb dev-env/.bashrc .
+ln -sb dev-env/.bash_ind .
+ln -sb dev-env/.screenrc .
 ln -sb dev-env/.vimrc .
 ln -sb dev-env/.jshintrc .
